6e55806871e210b521d7052e78f595e27081774f
DArren <DArren@DArren-PC.home>
2016-04-09 03:00:23 +0100

    adasd

--- src/tests/TestDejongs.java
+++ src/tests/TestDejongs.java
@@ -17,6 +17,7 @@ public class TestDejongs {
 	
 	@Test
 	public void testIntN5Minimize() {
+		long start = System.currentTimeMillis();
 		Ga bpso = new Ga(40, SumIntFunctionN5.NUM_DIMENSIONS);
 		SumIntFunctionN5 goodness =  new SumIntFunctionN5();
 		bpso.optimize(10000, goodness, false);		
@@ -24,53 +25,62 @@ public class TestDejongs {
 		for (int i=0; i < SumIntFunctionN5.VARIABLES; i++) {
 			assertTrue( Math.abs(SumIntFunctionN5.MIN - goodness.getN(0)) < 0.5);
 		}
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken for: " + (end - start) + " TestIntN5Minimize ---------");
 	}
-
+	
 	@Test
 	public void testSphereMinimize() {
+		long start = System.currentTimeMillis();
 		Ga bpso = new Ga(40, SphereFunction.NUM_DIMENSIONS);
 		SphereFunction goodness =  new SphereFunction();
 		bpso.optimize(1000, goodness, false);
 		
-		System.out.println("Particle value: " + Population.getValue(Population.bestGlobal().getBits()));
-		System.out.println("Particle bit string: " + Arrays.toString(Population.bestGlobal().getBits()));
-		System.out.println("Particle goodness: " + goodness.fitness(Population.bestGlobal().getBits()));
-		System.out.println("----" + Math.abs(SphereFunction.MIN - goodness.getValue(Population.bestGlobal().getBits())));
-
+//		System.out.println("Particle value: " + Particle.getValue(Particle.bestGlobal()));
+//		System.out.println("Particle bit string: " + Arrays.toString(Particle.bestGlobal()));
+//		System.out.println("Particle goodness: " + goodness.getGoodness(Particle.bestGlobal()));
 		assertTrue( Math.abs(SphereFunction.MIN - goodness.getValue(Population.bestGlobal().getBits())) < 0.5);
+		
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken for: " + (end - start) + " Test Sphere minimize ---------");
 	}
 	
 	@Test
 	public void testSphereMaximize() {
+		long start = System.currentTimeMillis();
 		Ga bpso = new Ga(40, SphereFunction.NUM_DIMENSIONS);
 		SphereFunction goodness =  new SphereFunction();
-
 		bpso.optimize(1000, goodness, true);
 		
-		System.out.println("Particle value: " + Population.getValue(Population.bestGlobal().getBits()));
-		System.out.println("Particle bit string: " + Arrays.toString(Population.bestGlobal().getBits()));
-		System.out.println("Particle goodness: " + goodness.fitness(Population.bestGlobal().getBits()));
-		System.out.println("----" + Math.abs(SphereFunction.MAX - goodness.fitness(Population.bestGlobal().getBits())));
-		assertTrue( Math.abs(SphereFunction.MAX - goodness.fitness(Population.bestGlobal().getBits())) < 0.5
+//		System.out.println("Particle value: " + Particle.getValue(Particle.bestGlobal()));
+//		System.out.println("Particle bit string: " + Arrays.toString(Particle.bestGlobal()));
+//		System.out.println("Particle goodness: " + goodness.getGoodness(Particle.bestGlobal()));
+		assertTrue( Math.abs(SphereFunction.MAX - goodness.getValue(Population.bestGlobal().getBits())) < 0.5
 				    || Math.abs(-SphereFunction.MAX - goodness.getValue(Population.bestGlobal().getBits())) < 0.5);
+		
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken for: " + (end - start) + " Test Sphere Maximize ---------");
 	}
 	
 	@Test
 	public void testSphereMinimizeN3() {
+		long start = System.currentTimeMillis();
 		Ga bpso = new Ga(40, SphereFunctionN3.NUM_DIMENSIONS);
 		SphereFunctionN3 goodness =  new SphereFunctionN3();
 
 		bpso.optimize(10000, goodness, false);
-		
-		System.out.println("Particle value: " + Population.getValue(Population.bestGlobal().getBits()));
-		System.out.println("Particle bit string: " + Arrays.toString(Population.bestGlobal().getBits()));
-		System.out.println("Particle goodness: " + goodness.fitness(Population.bestGlobal().getBits()));
-		System.out.println("Particle goodness N1: " + goodness.getN1());
-		System.out.println("Particle goodness N2: " + goodness.getN2());
-		System.out.println("Particle goodness N3: " + goodness.getN3());
+//		
+//		System.out.println("Particle value: " + Particle.getValue(Particle.bestGlobal()));
+//		System.out.println("Particle bit string: " + Arrays.toString(Particle.bestGlobal()));
+//		System.out.println("Particle goodness: " + goodness.getGoodness(Particle.bestGlobal()));
+//		System.out.println("Particle goodness N1: " + goodness.getN1());
+//		System.out.println("Particle goodness N2: " + goodness.getN2());
+//		System.out.println("Particle goodness N3: " + goodness.getN3());
 		
 		assertTrue( Math.abs(SphereFunctionN3.MIN - goodness.getN1()) < 0.5);
 		assertTrue( Math.abs(SphereFunctionN3.MIN - goodness.getN2()) < 0.5);
 		assertTrue( Math.abs(SphereFunctionN3.MIN - goodness.getN3()) < 0.5);
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken for: " + (end - start) + " Test Sphere MinimizeN3 ---------");
 	}
 }
